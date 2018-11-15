fc0bb01ad6bffe026139d609eb1d4d18e7a62317
Jan Fincke <jan.fincke@cadworks.fi>
2018-11-15 15:04:13 +0200

    Removed tkinter launcher for now

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from Tkinter import Tk
+from updater import ArcDpsUpdater
 
-from views import Window
-
-root = Tk()
-# size of the window
-root.geometry("400x100")
-# make not resizeable
-root.resizable(False, False)
-
-app = Window(root)
-root.mainloop()
+updater = ArcDpsUpdater()
+updater.check_for_updates()
