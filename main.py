df450b02439f13f9f8bea9a12282aa5bca1d95e0
Jan Fincke <janfincke1@gmail.com>
2019-03-16 18:01:02 +0200

    Added program stop on error

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import logging
+import traceback
 from updater import ArcDpsUpdater
-
-updater = ArcDpsUpdater()
-updater.check_for_updates()
+try:
+    updater = ArcDpsUpdater()
+    updater.check_for_updates()
+except Exception as e:
+    logging.error(traceback.format_exc())
+    raw_input('Press any key to continue...')
+    raise SystemExit
